2e8b9d5d0a05007472d872b7304cbe7e82c0fd4d
John Ejike-Nwogu <jejikenwogu@zynga.com>
2016-07-04 00:12:02 -0700

    another commit

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,19 @@
-$( window ).scroll(function() {
-    if ($(window).scrollTop() > 0) {
-        $('.nav-focus').addClass("nav-scrolled");
-    }
-    else {
-        $('.nav-focus').removeClass("nav-scrolled");
-    }
-});
-$(function(){
-    $('.carousel').carousel({
-        interval: 2000
-    });
+$(document).ready(function() {
+    var controller = new ScrollMagic.Controller();
+
+    var mainScene = new ScrollMagic.Scene({
+    	triggerElement: '#doubleImage',
+    	triggerHook: .999
+    })
+    .setClassToggle('#navbar', 'nav-scrolled')
+    .addIndicators()
+    .addTo(controller);
+
+    var nextScene = new ScrollMagic.Scene({
+    	triggerElement: '#doubleImage',
+    	triggerHook: 1,
+    	duration: '100%'
+    })
+    .setTween(TweenMax.to('.carousel-inner', 1, {y: '30%', ease: Power0.easeNone}))
+    .addTo(controller);
 });
